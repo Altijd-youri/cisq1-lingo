a6a5c2121abb64947c417cf219f2c64ccfcaf6b4
Altijd_Youri <advertentiey@vanderstar.eu>
2021-02-19 17:42:37 +0100

    Added basic implementation of Feedback.compare() - create marks based on answer and guess - red phase

--- src/main/java/nl/hu/cisq1/lingo/trainer/domain/Feedback.java
+++ src/main/java/nl/hu/cisq1/lingo/trainer/domain/Feedback.java
@@ -1,14 +1,43 @@
 package nl.hu.cisq1.lingo.trainer.domain;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Feedback {
     private final List<Mark> marks;
     private final String guess;
+    private final String answer;
 
-    Feedback(String guess, List<Mark> marks) {
+    Feedback(String guess, String answer) {
         this.guess = guess;
-        this.marks = marks;
+        this.answer = answer;
+        this.marks = this.compare();
+    }
+
+    private List<Mark> compare() {
+        List<Mark> marks = new ArrayList<>();
+        String guessCharaters = guess;
+        for (int answerPosition = 0; answerPosition < answer.length(); answerPosition++) {
+            Character answerletter = answer.charAt(answerPosition);
+            for (int guessPosition = 0; guessPosition < guess.length(); guessPosition++) {
+                Character guessLetter = guess.charAt(answerPosition);
+
+                if (answerletter.equals(guessLetter)) {
+                    marks.add(Mark.CORRECT);
+                    continue;
+                }
+                if (answer.contains(guessCharaters)) {
+                    marks.add(Mark.PRESENT);
+                    continue;
+                }
+                marks.add(Mark.INCORRECT);
+            }
+        }
+        return marks;
+    }
+
+    public List<Mark> getMarks() {
+        return marks;
     }
 
     public boolean isWordGuessed() {
--- src/test/java/nl/hu/cisq1/lingo/trainer/domain/FeedbackTest.java
+++ src/test/java/nl/hu/cisq1/lingo/trainer/domain/FeedbackTest.java
@@ -8,33 +8,33 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 class FeedbackTest {
-    private final String WORD = "BOMEN";
+    private final String WORD = "BAARD";
 
     @Test
     @DisplayName("Word is guessed if all letters are correct")
     void wordIsGuessed() {
-        Feedback feedback = new Feedback(WORD, List.of(Mark.CORRECT, Mark.CORRECT, Mark.CORRECT, Mark.CORRECT, Mark.CORRECT));
+        Feedback feedback = new Feedback(WORD, WORD);
         assertTrue(feedback.isWordGuessed());
     }
 
     @Test
     @DisplayName("Word is not guessed if all not letters are correct")
     void wordIsNotGuessed() {
-        Feedback feedback = new Feedback(WORD, List.of(Mark.CORRECT, Mark.INCORRECT, Mark.CORRECT, Mark.CORRECT, Mark.CORRECT));
+        Feedback feedback = new Feedback(WORD, WORD.substring(0,3)+"A");
         assertFalse(feedback.isWordGuessed());
     }
 
     @Test
     @DisplayName("Word is valid if all letters are valid")
     void wordIsValid() {
-        Feedback feedback = new Feedback(WORD, List.of(Mark.CORRECT, Mark.INCORRECT, Mark.CORRECT, Mark.CORRECT, Mark.CORRECT));
+        Feedback feedback = new Feedback(WORD, WORD);
         assertTrue(feedback.isWordValid());
     }
 
     @Test
     @DisplayName("Word is invalid if all letters are invalid")
     void wordIsInvalid() {
-        Feedback feedback = new Feedback(WORD, List.of(Mark.INVALID, Mark.INVALID, Mark.INVALID, Mark.INVALID, Mark.INVALID));
+        Feedback feedback = new Feedback(WORD, WORD+"IN");
         assertFalse(feedback.isWordValid());
     }
 }
