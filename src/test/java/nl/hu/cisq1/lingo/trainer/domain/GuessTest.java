b21aed4641cf4cbaf7e5e7f9b21038b078cc588e
Altijd_Youri <advertentiey@vanderstar.eu>
2021-03-26 13:19:03 +0100

    Refactored old Guess tests and added new test to test changes form the last commit.

--- src/test/java/nl/hu/cisq1/lingo/trainer/domain/GuessTest.java
+++ src/test/java/nl/hu/cisq1/lingo/trainer/domain/GuessTest.java
@@ -1,33 +1,62 @@
 package nl.hu.cisq1.lingo.trainer.domain;
 
+import nl.hu.cisq1.lingo.trainer.domain.exceptions.InvalidGuessException;
+import nl.hu.cisq1.lingo.trainer.domain.exceptions.NoValidRoundException;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
-
-import java.util.stream.Stream;
-
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class GuessTest {
+    private Game game;
 
-    private static Stream<Arguments> hintTestcases() {
-        Round round = new Round(1,"BAARD");
-        return Stream.of(
-                Arguments.of(round, "B....", "BONJE"),
-                Arguments.of(round, "BA...", "BARST"),
-                Arguments.of(round, "BAA.D", "DRAAD"),
-                Arguments.of(round, "BAARD", "BAARD")
-        );
+    @BeforeEach
+    @DisplayName("Create a game once.")
+    void createGameTest() {
+        this.game = new Game();
+        this.game.newRound("BAARD");
     }
 
-    @ParameterizedTest
-    @MethodSource("hintTestcases")
-    @DisplayName("Hint is correct when it match the expected hint.")
-    void hint(Round round, String correctHint, String guessedWord) {
-        Guess guess = new Guess(round, guessedWord);
-        assertEquals(correctHint, guess.takeAGuess());
+    @Test
+    @DisplayName("Hint is correct when it matches the expected hint.")
+    void orderedHintTest() {
+        doPartOfOrderedGuessTest("B....", "BONJE");
+        doPartOfOrderedGuessTest("BA...", "BARST");
+        doPartOfOrderedGuessTest("BAA.D", "DRAAD");
+        doPartOfOrderedGuessTest("BAARD", "BAARD");
     }
 
+    private void doPartOfOrderedGuessTest(String correct, String guess) {
+        try {
+            assertEquals(correct, game.guessWord(guess));
+        } catch (NoValidRoundException | InvalidGuessException e) {
+            fail("Should not throw "+e.getClass().getName());
+        }
+    }
 
+    @Test
+    @DisplayName("Check if Word is guessed.")
+    void wordIsGuessedTest() {
+        try {
+            assertEquals("BAARD", game.guessWord("BAARD"));
+        } catch (NoValidRoundException | InvalidGuessException e) {
+            fail("Should not throw "+e.getClass().getName());
+        }
+    }
+
+    @Test
+    @DisplayName("Invalid word throw Exception")
+    void wordIsInvalidTest() {
+        assertThrows(InvalidGuessException.class, () -> game.guessWord("LANGERDANBAARD"));
+        assertThrows(InvalidGuessException.class, () ->  game.guessWord("LBAARD"));
+    }
+
+    @Test
+    @DisplayName("Invalid round throw Exception")
+    void roundIsInvalidTest() {
+        this.game = new Game(); //create a game without a round;
+
+        assertThrows(NoValidRoundException.class, () -> game.guessWord("BONJE"));
+        assertThrows(NoValidRoundException.class, () ->  game.guessWord("BAARD"));
+    }
 }
