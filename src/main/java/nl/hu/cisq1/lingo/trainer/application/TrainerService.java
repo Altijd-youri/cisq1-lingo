3c5a011bc9fb5b9d53827fd7ef93410d212bdb76
Altijd_Youri <advertentiey@vanderstar.eu>
2021-04-05 12:14:39 +0200

    [draft] Basic implementation of TrainerService.

---
+++ src/main/java/nl/hu/cisq1/lingo/trainer/application/TrainerService.java
@@ -0,0 +1,59 @@
+package nl.hu.cisq1.lingo.trainer.application;
+
+import nl.hu.cisq1.lingo.trainer.data.SpringGameRepository;
+import nl.hu.cisq1.lingo.trainer.domain.Game;
+import nl.hu.cisq1.lingo.trainer.domain.exceptions.InvalidGuessException;
+import nl.hu.cisq1.lingo.trainer.domain.exceptions.NoActiveRoundException;
+import nl.hu.cisq1.lingo.trainer.domain.exceptions.PreviousRoundNotFinishedException;
+import nl.hu.cisq1.lingo.words.application.WordService;
+import org.springframework.stereotype.Service;
+
+import javax.management.InstanceNotFoundException;
+import javax.transaction.Transactional;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+@Transactional
+public class TrainerService {
+    private WordService wordService;
+
+    private SpringGameRepository gameRepository;
+
+
+    public TrainerService(WordService wordService, SpringGameRepository gameRepository) {
+        this.wordService = wordService;
+        this.gameRepository = gameRepository;
+    }
+
+    public void startNewGame() {
+        Game game = new Game();
+
+        gameRepository.save(game);
+    }
+
+    public void startNewRound(UUID uuid) throws InstanceNotFoundException, NoActiveRoundException, PreviousRoundNotFinishedException {
+        Optional<Game> optionalGame = gameRepository.findById(uuid);
+        if (!optionalGame.isPresent()) throw new InstanceNotFoundException("Game with given Id doesn't exsists.");
+
+        Game game = optionalGame.get();
+
+        int length = 5; //TODO - Implement length grow and shrink cycle.
+        String word = wordService.provideRandomWord(length);
+
+        game.newRound(word);
+
+        gameRepository.save(game);
+    }
+
+    public void guessWord(UUID uuid, String guess) throws InstanceNotFoundException, NoActiveRoundException, InvalidGuessException {
+        Optional<Game> optionalGame = gameRepository.findById(uuid);
+        if (!optionalGame.isPresent()) throw new InstanceNotFoundException("Game with given Id doesn't exsists.");
+
+        Game game = optionalGame.get();
+
+        game.guessWord(guess);
+
+        gameRepository.save(game);
+    }
+}
