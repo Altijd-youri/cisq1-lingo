ca952e930e907aa480fa95232ba21e3dd60641db
Altijd_Youri <advertentiey@vanderstar.eu>
2021-04-07 18:36:30 +0200

    Bug fix: Removed unused Exception. And added manual run option to Github Actions workflow.

--- src/main/java/nl/hu/cisq1/lingo/trainer/domain/exceptions/MaxRoundsReachedException.java
+++
@@ -1,7 +0,0 @@
-package nl.hu.cisq1.lingo.trainer.domain.exceptions;
-
-public class MaxRoundsReachedException extends Exception {
-    public MaxRoundsReachedException() {
-        super("Game already reached the maximum amount of playable rounds.");
-    }
-}
